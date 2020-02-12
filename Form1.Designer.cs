4c2d9affe1d4e0144e8f3f9223609ac1bff9e093
javiervalpad <javiervalpad@monlau.com>
2020-02-12 15:56:51 +0100

    Version 0.0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PracticaPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDNI = new System.Windows.Forms.TextBox();
+            this.labelDNI = new System.Windows.Forms.Label();
+            this.textBoxDNILetter = new System.Windows.Forms.TextBox();
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Location = new System.Drawing.Point(189, 91);
+            this.textBoxDNI.MaxLength = 8;
+            this.textBoxDNI.Multiline = true;
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(163, 22);
+            this.textBoxDNI.TabIndex = 0;
+            // 
+            // labelDNI
+            // 
+            this.labelDNI.AutoSize = true;
+            this.labelDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDNI.Location = new System.Drawing.Point(130, 91);
+            this.labelDNI.Name = "labelDNI";
+            this.labelDNI.Size = new System.Drawing.Size(41, 18);
+            this.labelDNI.TabIndex = 1;
+            this.labelDNI.Text = "DNI:";
+            // 
+            // textBoxDNILetter
+            // 
+            this.textBoxDNILetter.Location = new System.Drawing.Point(376, 91);
+            this.textBoxDNILetter.Multiline = true;
+            this.textBoxDNILetter.Name = "textBoxDNILetter";
+            this.textBoxDNILetter.Size = new System.Drawing.Size(31, 22);
+            this.textBoxDNILetter.TabIndex = 2;
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAccept.Location = new System.Drawing.Point(162, 196);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(231, 35);
+            this.buttonAccept.TabIndex = 3;
+            this.buttonAccept.Text = "Accept";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(555, 307);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.textBoxDNILetter);
+            this.Controls.Add(this.labelDNI);
+            this.Controls.Add(this.textBoxDNI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDNI;
+        private System.Windows.Forms.Label labelDNI;
+        private System.Windows.Forms.TextBox textBoxDNILetter;
+        private System.Windows.Forms.Button buttonAccept;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAccept_Click(object sender, EventArgs e)
+        {
+            int DNI = Convert.ToInt32(textBoxDNI.Text);
+            char DNIletter = functionDNI(DNI);
+            textBoxDNILetter.Text = DNIletter.ToString();
+        }
+
+        private char functionDNI(int DNI)
+        {
+            char DNIletter = 'A';
+
+            return DNIletter;
+            
+
+
+        }
+
+    }
+}
